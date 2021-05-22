e8bd8dc8386564cfcf05dbbdd63ab3a2d0484353
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2021-05-22 21:29:44 +0300

    taskValidator correction

--- app/Http/Requests/TaskValidator.php
+++ app/Http/Requests/TaskValidator.php
@@ -32,7 +32,7 @@ class TaskValidator extends FormRequest
 
             'key_range'=>'numeric|nullable',
             'start_date'=>'required|date',
-            'end_date'=>'required|date|after:start_date',
+            'end_date'=>'required|date|after_or_equal:start_date',
             ];
     }
 
